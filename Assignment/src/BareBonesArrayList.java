46dbf8493e175b804f176593579db6cffa2be59f
ikeikeken <khuynh34@toromail.csudh.edu>
2026-09-21 21:02:30 -0700

    homeworking

---
+++ Assignment/src/BareBonesArrayList.java
@@ -0,0 +1,11 @@
+//We list all the methods that we need in AL here
+public interface BareBonesArrayList <E>
+{
+    public void add(E a); //This methods adds to the end of the list
+    public void addE(E a,int index); // Adds at specified index
+    public E remove(int index); // Returns t
+    public E get(int index); // Returns the element without deleting
+    public void set (E a, int index); // Update the element
+    public int getSize(); // Tells how many element size
+    public int indexOf(E a); //Search for element, return -1 if not found
+}
---
+++ Assignment/src/DHarrayList.java
@@ -0,0 +1,45 @@
+public class DHarrayList<E> implements BareBonesArrayList
+{
+    @Override
+    public void add(E a)
+    {
+        //TODO Auto-generated method stub
+    }
+
+    @Override
+    public void add(E a, int index)
+    {
+        //TODO Auto-generated method stub
+    }
+
+    @Override
+    public E remove(int index)
+    {
+        //TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public E get(int index)
+    {
+        //TODO Auto-generated method stub
+    }
+
+    @Override
+    public void set (E a, int index)
+    {
+        //TODO Auto-generated method stub
+    }
+
+    @Override
+    public int getSize()
+    {
+        //TODO Auto-generated method stub
+    }
+
+    @Override
+    public int indexOf(E a)
+    {
+        //TODO Auto-generated method stub
+    }
+}
